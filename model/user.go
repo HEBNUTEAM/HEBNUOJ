3e0945769e146c5a78af826576a75df5c3d87594
量子态薛定谔 <maxmalmap@qq.com>
2021-02-02 23:16:47 +0800

    优化

--- model/user.go
+++ model/user.go
@@ -10,7 +10,7 @@ type User struct {
 	Defunct    bool      `gorm:"not null"`
 	Ip         string    `gorm:"type:varchar(48);not null"`
 	CreateTime time.Time `gorm:"type:datetime;autoCreateTime;not null"`
-	Password   string    `gorm:"type:size:60;not null"`
+	Password   string    `gorm:"size:60;not null"`
 	NickName   string    `gorm:"type:varchar(25);not null"`
 	School     string    `gorm:"type:varchar(25);"`
 	Role       string    `gorm:"type:varchar(15);not null"`
