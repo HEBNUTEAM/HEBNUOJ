db84f9d154b52826682bf405939a9abc8e0123b5
量子态薛定谔 <maxmalmap@qq.com>
2021-02-02 21:53:15 +0800

    优化user表设计

--- model/user.go
+++ model/user.go
@@ -4,14 +4,14 @@ import "time"
 
 type User struct {
 	Id         int    `gorm:autoIncrement;primaryKey`
-	Email      string `gorm:"type:varchar(100);uniqueIndex"`
+	Email      string `gorm:"type:varchar(30);uniqueIndex"`
 	Submit     int
 	Solved     int
 	Defunct    bool      `gorm:"not null"`
 	Ip         string    `gorm:"type:varchar(48);not null"`
 	CreateTime time.Time `gorm:"type:datetime;autoCreateTime;not null"`
-	Password   string    `gorm:"type:varchar(20);not null"`
-	NickName   string    `gorm:"type:varchar(100);not null"`
-	School     string    `gorm:"type:varchar(100);"`
-	Role       string    `gorm:"type:varchar(20);not null"`
+	Password   string    `gorm:"type:size:60;not null"`
+	NickName   string    `gorm:"type:varchar(25);not null"`
+	School     string    `gorm:"type:varchar(25);"`
+	Role       string    `gorm:"type:varchar(15);not null"`
 }
