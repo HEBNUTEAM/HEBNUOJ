c8f3f18bfae125b61a2b8f53c6dfb3c406875754
量子态薛定谔 <maxmalmap@qq.com>
2021-02-03 09:58:02 +0800

    封装http response函数

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -4,6 +4,7 @@ import (
 	"github.com/HEBNUOJ/common"
 	"github.com/HEBNUOJ/dto"
 	"github.com/HEBNUOJ/model"
+	"github.com/HEBNUOJ/response"
 	"github.com/HEBNUOJ/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
@@ -22,41 +23,28 @@ func Register(ctx *gin.Context) {
 	password2 := ctx.PostForm("pwd2")
 
 	if len(nickname) > 25 || len(nickname) == 0 {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
-			"code": 422,
-			"msg":  "用户名的长度必须大于等于1个字符，小于等于25个字符",
-		})
+		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil,
+			"用户名的长度必须大于等于1个字符，小于等于25个字符")
 		return
 	}
 
 	if len(password1) < 6 {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
-			"code": 422,
-			"msg":  "密码不能小于6位",
-		})
+		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "密码不能小于6位")
+
 		return
 	}
 
 	if password1 != password2 {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
-			"code": 422,
-			"msg":  "两次密码不一致",
-		})
+		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "两次密码不一致")
 		return
 	}
 	if isEmailExist(db, email) {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
-			"code": 422,
-			"msg":  "邮箱已存在",
-		})
+		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "邮箱已存在")
 		return
 	}
 	hasedPassword, err := bcrypt.GenerateFromPassword([]byte(password1), bcrypt.DefaultCost)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"code": 500,
-			"msg":  "加密错误",
-		})
+		response.Response(ctx, http.StatusInternalServerError, 500, nil, "加密错误")
 		return
 	}
 	ip := ctx.ClientIP()
@@ -76,9 +64,7 @@ func Register(ctx *gin.Context) {
 		Role:       "common",
 	}
 	db.Create(&newUser)
-	ctx.JSON(200, gin.H{
-		"msg": "注册成功",
-	})
+	response.Success(ctx, nil, "注册成功")
 }
 
 func Login(ctx *gin.Context) {
@@ -92,44 +78,30 @@ func Login(ctx *gin.Context) {
 	var user model.User
 	db.Where("email = ?", email).First(&user)
 	if user.Id == 0 {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
-			"code": 422,
-			"msg":  "用户不存在或邮箱错误",
-		})
+		response.Response(ctx, http.StatusUnprocessableEntity, 422, nil, "用户不存在或邮箱错误")
 		return
 	}
 	// 判断密码是否正确
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"code": 400,
-			"msg":  "密码错误",
-		})
+		response.Response(ctx, http.StatusBadRequest, 400, nil, "密码错误")
 		return
 	}
 
 	// 发放token给前端
 	token, err := common.ReleaseToken(user)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"code": 500,
-			"msg":  "系统异常",
-		})
+		response.Response(ctx, http.StatusInternalServerError, 500, nil, "系统异常")
+
 		utils.Log("token.log", 5).Println(err) // 记录错误日志
 		return
 	}
-	ctx.JSON(200, gin.H{
-		"code": 200,
-		"data": gin.H{"token": token},
-		"msg":  "登陆成功",
-	})
+	response.Success(ctx, gin.H{"token": token}, "登陆成功")
 }
 
 func Info(ctx *gin.Context) {
 	user, _ := ctx.Get("user")
-	ctx.JSON(http.StatusOK, gin.H{
-		"code": 200,
-		"data": gin.H{"user": dto.ToUserDto(user.(model.User))},
-	})
+	response.Success(ctx, gin.H{"user": dto.ToUserDto(user.(model.User))}, "")
+
 }
 
 func isEmailExist(db *gorm.DB, email string) bool {
