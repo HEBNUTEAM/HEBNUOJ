fb0c9b4954101e7616489038aeb3ae040451a257
量子态薛定谔 <maxmalmap@qq.com>
2021-02-06 23:20:54 +0800

    验证码路由

--- router/routes.go
+++ router/routes.go
@@ -6,10 +6,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func CollectRoute(r *gin.Engine) *gin.Engine {
+func CollectRegisterAndLoginRoute(r *gin.Engine) *gin.Engine {
 	r.Use(middleware.CorsMiddleware())
 	r.POST("/api/auth/register", controller.Register)
 	r.POST("/api/auth/login", controller.Login)
 	r.POST("/api/auth/info", middleware.AuthMiddleware(), controller.Info)
+	r.POST("/api/auth/")
+	return r
+}
+
+func CollectVerifyRoute(r *gin.Engine) *gin.Engine {
+	//VerifyRoute := r.Group("/verifycode")
+	checkCodeController := new(controller.CheckCodeController)
+	r.GET("/api/refresh", checkCodeController.ReloadVerifyCode)
+	r.GET("/api/show/:id", checkCodeController.GenVerifyCode)
+	r.GET("/api/verify", checkCodeController.VerifyCode)
 	return r
 }
