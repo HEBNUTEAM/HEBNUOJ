d86bcafba5d0f804097577bc0230c63b92b9198e
量子态薛定谔 <maxmalmap@qq.com>
2021-02-07 13:09:28 +0800

    增加图形验证码参数

--- vo/login_vo.go
+++ vo/login_vo.go
@@ -4,6 +4,7 @@ type LoginVo struct {
 	NickName     string `json:"nick"`
 	Email        string `json:"email"`
 	Verification string `json:"verification"`
+	Captcha      string `json:"captcha"`
 	Password1    string `json:"pwd"`
 	Password2    string `json:"pwd1"`
 }
